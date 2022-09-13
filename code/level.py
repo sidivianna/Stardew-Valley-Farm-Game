1a314b4bb26d5614106a23a22bcf817f867329de
sidivianna <sidineirochavianna19@gmail.com>
2022-09-13 07:08:55 -0300

    Reiniciando o dia

--- code/level.py
+++ code/level.py
@@ -1,11 +1,13 @@
 
+from code import interact
 import pygame
 from settings import *
 from player import Player
 from overlay import Overlay
-from sprites import Generic, Water, WildFlower, Tree
+from sprites import Generic, Water, WildFlower, Tree, Interaction
 from pytmx.util_pygame import load_pygame
 from support import *
+from transition import Transition
 
 class Level:
     def __init__(self):
@@ -17,9 +19,11 @@ class Level:
         self.all_sprites = CameraGroup() # troca
         self.collision_sprites = pygame.sprite.Group()
         self.tree_sprites = pygame.sprite.Group()
+        self.interaction_sprites = pygame.sprite.Group()
 
         self.setup()
         self.overlay = Overlay(self.player)
+        self.transition = Transition(self.reset, self.player)
 
     def setup(self):
         tmx_data = load_pygame('../Stardew Valley Game/data/map.tmx')
@@ -42,7 +46,7 @@ class Level:
         for x, y, surf in tmx_data.get_layer_by_name('Water').tiles():
             Water((x * TILE_SIZE, y * TILE_SIZE), water_frames, self.all_sprites)
 
-        #trees
+        # trees
         for obj in tmx_data.get_layer_by_name('Trees'):
             Tree(
                 pos = (obj.x, obj.y), 
@@ -67,8 +71,12 @@ class Level:
                     pos = (obj.x, obj.y), 
                     group = self.all_sprites, 
                     collision_sprites = self.collision_sprites,
-                    tree_sprites = self.tree_sprites
-                    )
+                    tree_sprites = self.tree_sprites,
+                    interaction = self.interaction_sprites)
+
+            if obj.name == 'Bed':
+                Interaction((obj.x,obj.y), (obj.width,obj.height), self.interaction_sprites, obj.name)
+
         
         Generic(
             pos = (0,0),
@@ -80,12 +88,23 @@ class Level:
 
         self.player.item_inventory[item] += 1
         
+    def reset(self):
+
+        # apples on the trees
+        for tree in self.tree_sprites.sprites():
+            for apple in tree.apple_sprites.sprites():
+                apple.kill()
+            tree.create_fruit()
+
     def run(self, dt):
         self.display_surface.fill('black')
         self.all_sprites.custom_draw(self.player)
         self.all_sprites.update(dt)
 
         self.overlay.display()
+
+        if self.player.sleep:
+            self.transition.play()
 
 class CameraGroup(pygame.sprite.Group):
     def __init__(self):
